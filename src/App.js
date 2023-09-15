96f9d30a4ab93d3e3f4b6106e84d9bb96722eb92
charankannag <charankanna2013@gmail.com>
2023-09-15 12:48:45 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React from 'react';
+
+import {Link,BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './home';
+import User from './user';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <div><h1 style={{textAlign:'center'}}>LIBRARY MANGEMENT SYSTEM</h1></div>
+      <div className='menu'>
+        <ul>
+          <li><Link to={"/book"}>Book Data</Link></li>
+          <li><Link to={"/user"}>User Data</Link></li>
+        </ul>
+        </div>
+      <div className='bookContent'>
+        <Routes>
+          <Route path='/book' element={<Home/>}></Route>
+        </Routes>
+      
+      </div>
+      <div className='userContent'>
+        <Routes>
+          <Route path='/user' element={<User/>}></Route>
+        </Routes>
+      
+      </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
